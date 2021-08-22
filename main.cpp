6f39120cbdd4c01239568b593e315aa0e3b5a6b1
Kentaro Seki <kentaro.kpc@gmail.com>
2021-08-22 16:58:25 +0900

    Abstクラス

--- main.cpp
+++ main.cpp
@@ -1,36 +1,59 @@
 #define F_CPU 1000000
-#include "firmware/led.hpp"
-#include "firmware/switch.hpp"
-#include "firmware/pwm.hpp"
-#include "firmware/callback.hpp"
+#include "avr_lib/led.hpp"
+#include "avr_lib/switch.hpp"
+#include "avr_lib/callback.hpp"
 #include <avr/io.h>
 #include <avr/sleep.h>
 #include <util/delay.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
-auto led_green = LED_D0();
-auto switch1 = SWITCH_D1();
-auto pwm = PWM0A();
+// Transistor
+auto T0 = LED_D5();
+auto T1 = LED_D6();
+auto T2 = LED_D7();
+auto T3 = LED_D2();
+auto T4 = LED_D3();
+
+// LED
+auto LED0 = LED_D0();
+auto LED1 = LED_C0();
+auto LED2 = LED_C1();
+auto LED3 = LED_C2();
+auto LED4 = LED_C3();
+auto LED5 = LED_C4();
+auto LED6 = LED_C5();
+
+// Swtich
+auto S0 = SWITCH_B1();
+auto S1 = SWITCH_B5();
+auto S2 = SWITCH_B3();
+auto S3 = SWITCH_B4();
 
 int main(void)
 {
-    uint8_t t = 0;
+    Callback::init();
+    T0.on();
+    T1.on();
+    T2.on();
 
     while(1){
     }
 }
 
-void SensorCallback()
+void Callback::SensorCallback()
 {
-    switch1.update();
+    S0.update();
+    S1.update();
+    S2.update();
+    S3.update();
 }
 
-void MainCallback()
+void Callback::MainCallback()
 {
-    if(switch1()){
-        led_green.on();
+    if(S0()){
+        LED0.on();
     }else{
-        led_green.off();
+        LED0.off();
     }
 }
